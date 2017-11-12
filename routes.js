a05f802f6dc8624cbf892aa843d64bec67e225c0
Edwin Chen <hello@echen.me>
2017-11-12 14:47:20 -0800

    ...

--- routes.js
+++ routes.js
@@ -1,24 +1,9 @@
 app.config(["$routeProvider", "$compileProvider", function($routeProvider, $compileProvider) {
     $routeProvider.    
-      when("/network", {
-        templateUrl: "network.html",
-        controller: "LSTMsController"
-      }).
-      when("/words", {
-        templateUrl: "words.html",
-        controller: "WordsController"
-      }).      
-      when("/neuron", {
-        templateUrl: "neuron.html",
-        controller: "NeuronsController"
-      }).
-      when("/about", {
-        templateUrl: "about.html",
-      }).
-      when("/gallery", {
-        templateUrl: "gallery.html",
+      when("/", {
+        templateUrl: "explorer.html",
       }).            
       otherwise({
-        redirectTo: "/gallery"
+        redirectTo: "/explorer"
       });
   }]);
